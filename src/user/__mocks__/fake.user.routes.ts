7019c573e575b336edc5034df94f909e6a2aa1f2
samsantosb <samsantosb@outlook.com>
2022-09-26 20:11:37 -0300

    update-repo

--- src/user/__mocks__/fake.user.routes.ts
+++ src/user/__mocks__/fake.user.routes.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 
 export const mockRequest = () => {
   const req = {} as Request;
-  req.body = jest.fn().mockReturnValue(req) as any as Request["body"];
-  req.params = jest.fn().mockReturnValue(req) as any as Request["params"];
-  req.query = jest.fn().mockReturnValue(req) as any as Request["query"];
+  req.body = jest.fn().mockReturnValue(req) as unknown as Request["body"];
+  req.params = jest.fn().mockReturnValue(req) as unknown as Request["params"];
+  req.query = jest.fn().mockReturnValue(req) as unknown as Request["query"];
   return req;
 };
 export const mockResponse = () => {
   const res = {} as Response;
-  res.status = jest.fn().mockReturnValue(res) as any as Response["status"];
-  res.json = jest.fn().mockReturnValue(res) as any as Response["json"];
+  res.status = jest.fn().mockReturnValue(res) as unknown as Response["status"];
+  res.json = jest.fn().mockReturnValue(res) as unknown as Response["json"];
   return res;
 };
